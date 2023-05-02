047a69740194073af3a039d3510aec7d58e57f76
Lewis-08 <lewis.babbage22@bathspa.ac.uk>
2023-05-02 22:10:00 +0100

    Adding comments and variable names

--- WebDevQuiz.js
+++ WebDevQuiz.js
@@ -1,12 +1,12 @@
 window.onload = function () {
   
-    var questionArea = document.getElementsByClassName('questions')[0],
-        answerArea   = document.getElementsByClassName('answers')[0],
-        checker      = document.getElementsByClassName('checker')[0],
+    var questionPosition = document.getElementsByClassName('Questions')[0],
+        answerPosition   = document.getElementsByClassName('Answers')[0],
+        checker      = document.getElementsByClassName('Checker')[0],
         current      = 0,
     
-       // An object that holds all the questions + possible answers.
-       // In the array --> last digit gives the right answer position
+       // an object that holds all of the questions and potential answers.
+       //  last number gives the position of the correct answer in the array
         allQuestions = {
           'Who Does Mike End Up Marrying?' : ['Donna', 'Rachel', 'Jessica', 1],
           
@@ -25,18 +25,18 @@ window.onload = function () {
     
       var question = Object.keys(allQuestions)[curr];
       
-      questionArea.innerHTML = '';
-      questionArea.innerHTML = question;    
+      questionPosition.innerHTML = '';
+      questionPosition.innerHTML = question;    
     }
     
-    function loadAnswers(curr) {
-    // This function loads all the possible answers of the given question
-    // It grabs the needed answer-array with the help of the current-variable
-    // Every answer is added with an 'onclick'-function
+    // Function that generates all of the possible answers of the question at hand
+    // It makes use of the needed answer-array with help from the current-variable
     
+    function loadAnswers(curr) {
+        
       var answers = allQuestions[Object.keys(allQuestions)[curr]];
       
-      answerArea.innerHTML = '';
+      answerPosition.innerHTML = '';
       
       for (var i = 0; i < answers.length -1; i += 1) {
         var createDiv = document.createElement('div'),
@@ -46,16 +46,17 @@ window.onload = function () {
         createDiv.addEventListener("click", checkAnswer(i, answers));
         
         
-        answerArea.appendChild(createDiv);
+        answerPosition.appendChild(createDiv);
       }
     }
     
+       // This function will run when one of the potential answers is clicked by the user
+      // It checks if the answer chosen by the user is the same as the correct answer
+      // The function then checks if the question is the last one within the quiz
+      // If so, the area where answers are displayed is cleared and the user is informed that the quiz is over
+
     function checkAnswer(i, arr) {
-      // This is the function that will run, when clicked on one of the answers
-      // Check if givenAnswer is sams as the correct one
-      // After this, check if it's the last question:
-      // If it is: empty the answerArea and let them know it's done.
-      
+         
       return function () {
         var givenAnswer = i,
             correctAnswer = arr[arr.length-1];
@@ -72,17 +73,17 @@ window.onload = function () {
           loadQuestion(current);
           loadAnswers(current);
         } else {
-          questionArea.innerHTML = 'Quiz Complete! View Your Score Below:';
-          answerArea.innerHTML = '';
+          questionPosition.innerHTML = 'Quiz Complete! View Your Score Below:';
+          answerPosition.innerHTML = '';
         }
                                 
       };
     }
     
+     // This function adds a div element to the page
+    // This div element is used to see if each answer selected is correct or false
     function addChecker(bool) {
-    // This function adds a div element to the page
-    // Used to see if it was correct or false
-    
+       
       var createDiv = document.createElement('div'),
           txt       = document.createTextNode(current + 1);
       
@@ -99,7 +100,7 @@ window.onload = function () {
     }
     
     
-    // Start the quiz right away
+    // Allows the user to start the quiz as soon as they load the webpage
     loadQuestion(current);
     loadAnswers(current);
     
